4fca500ff6d875d66191f3239202da33a1cb51d3
Arash <arashabbasi@Arashs-MacBook-Pro.local>
2019-12-02 13:52:45 -0800

    Reconfig eslint file using eslint --init command

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,25 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends" : "airbnb-base"
- }
- 
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
