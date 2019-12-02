5479dbfe998a3af62cde8ffb763c4345e31e6cb7
Arash <arashabbasi@Arashs-MacBook-Pro.local>
2019-12-02 13:45:02 -0800

    Add react to eslint config file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const App = (props) => {
-  return(
-    <div>
-      Hello from App.jsx
-    </div>
-  )
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Hello from App.jsx
+      </div>
+    );
+  }
+}
 
 export default App;
