9c5015a15e83eb072b19f7a1501e615a2084bb19
Arash Abbasi <s.arash.abbasi@gmail.com>
2019-12-03 15:49:38 -0800

    Add jest to eslint env

--- client/src/components/App.test.jsx
+++ client/src/components/App.test.jsx
@@ -1,6 +1,6 @@
+/* eslint-disable import/extensions */
 import React from 'react';
-import { shallow} from 'enzyme';
-import { configure } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from './App.jsx';
 
