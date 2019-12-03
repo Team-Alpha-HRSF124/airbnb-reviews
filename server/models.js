3d48d4fbd51bcd914b6ad3c968ddeeb725fe0a8d
Arash <arashabbasi@Arashs-MacBook-Pro.local>
2019-12-02 19:19:29 -0800

    Modify the get request for listing to handle async queries for stats and reviews

--- server/models.js
+++ server/models.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const mysql = require('mysql');
+const async = require('async');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -35,10 +36,10 @@ module.exports = {
   },
   listings: (d) => {
     const queryVal = [d.title, d.avg_rating, d.communication,
-      d.check_in, d.accuracy, d.value,
-      d.cleanliness,
-      d.hospitality, d.stylish, d.sparkling_clean,
-      d.quick_responses, d.amazing_amenities, d.counts, d.owners_id];
+    d.check_in, d.accuracy, d.value,
+    d.cleanliness,
+    d.hospitality, d.stylish, d.sparkling_clean,
+    d.quick_responses, d.amazing_amenities, d.counts, d.owners_id];
 
     const quuery = 'INSERT INTO listings (title, avg_rating, communication, check_in, accuracy, value, cleanliness, hospitality, stylish,sparkling_clean, quick_responses, amazing_amenities, counts, owners_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
 
@@ -63,7 +64,7 @@ module.exports = {
   },
   ownersResponses: (fakeResponses) => {
     const queryVal = [fakeResponses.response,
-      fakeResponses.date, fakeResponses.reviews_id, fakeResponses.owners_id];
+    fakeResponses.date, fakeResponses.reviews_id, fakeResponses.owners_id];
     const quuery = 'INSERT INTO owners_responses (response, date, reviews_id, owners_id) VALUES(?, ?, ?, ?)';
     connection.query(quuery, queryVal, (err, data) => {
       if (err) {
@@ -74,13 +75,30 @@ module.exports = {
     });
   },
   getListing: (callback, id) => {
-    const query = `select * from listings where id = ${id.id}`;
-    connection.query(query, (err, data) => {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, data);
-      }
+    const query1 = `select * from listings where id = ${id.id}`;
+    const query2 = `select * from reviews where listings_id = ${id.id}`;
+    const returnedData = {};
+
+    async.parallel([(parallelDone) => {
+      connection.query(query1, {}, (err, results) => {
+        if (err) {
+          parallelDone(err);
+        }
+        returnedData.listing = results;
+        parallelDone();
+      });
+    }, (parallelDone) => {
+      connection.query(query2, {}, (err, results) => {
+        if (err) {
+          parallelDone(err);
+        }
+        returnedData.reviews = results;
+        parallelDone();
+      });
+    }], (err) => {
+      if (err) console.log(err);
+      connection.end();
+      callback(null, returnedData);
     });
   },
 };
