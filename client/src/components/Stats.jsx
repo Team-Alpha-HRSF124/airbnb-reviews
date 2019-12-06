bbc9e06dea9948dcb43de93ed36f568e78d7ef08
Arash Abbasi <s.arash.abbasi@gmail.com>
2019-12-05 19:38:49 -0800

    Add MVP style for stats box

--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -16,22 +16,54 @@ size: 24px;
 color: #484848; 
 `;
 
-const ReviewsHeader = styled.div`
-  display: grid;
-  grid-template-columns: .5fr .5fr 4fr;
-  grid-auto-rows: 35px;
-  border: #333 1px solid;
-  border-radius: 10px;
+const NumFont = styled.span`
+font-family: 'Varela Round', sans-serif;
+size: 14px;
+color: #484848; 
 `;
 
-const Items = styled.span`
-  flex-grow: 2;
+const ReviewsHeader = styled.div`
+display: grid;
+grid-template-columns: repeat(6, 1fr);
+grid-template-rows: 1fr;
+grid-column-gap: 20px
+grid-row-gap: 20px
+justify-items: stretch
+align-items: start
+`;
+
+const OuterContainer = styled.div`
+display: grid;
+width: 600px;
+height: 200px;
+grid-template-rows: 49% 2% 49%;
+padding: 10px;
+border: 1px solid #ededed;
+border-radius: 10px;
+-moz-box-shadow: 0 0 3px #ccc;
+-webkit-box-shadow: 0 0 3px #ccc;
+box-shadow: 0 0 3px #ccc;
+`;
+
+const MidLine = styled.div`
+border-bottom: 1px solid #BEBEBE;
+`;
+
+const InnerContainer = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr;
+grid-template-rows: 1fr 1fr 1fr;
+font-family: 'Varela Round', sans-serif;
+padding: 10px;
+size: 18px;
+color: #484848; 
 `;
 
 const Stats = ({
   listing: {
-    id, title, avg_rating, communication, cleanliness,
-    hospitality, stylish, sparkling_clean, quick_responses, amazing_amenities, counts, check_in, accuracy, value, location,
+    avg_rating, communication, cleanliness,
+    hospitality, stylish, sparkling_clean, quick_responses,
+    amazing_amenities, counts, check_in, accuracy, value, location,
   },
 }) => (
   <WrappReviews>
@@ -40,29 +72,39 @@ const Stats = ({
       <ReviewsHeader>
         <div>
           <IoMdStar />
-          {avg_rating}
+          <NumFont>
+            {avg_rating}
+          </NumFont>
         </div>
         <div>
-          <div>
-            {counts}
-            Reviews
-          </div>
+          <NumFont>
+            <div>
+              {counts}
+              &nbsp;
+                Reviews
+            </div>
+          </NumFont>
         </div>
       </ReviewsHeader>
     </IconContext.Provider>
-      <div>ID: {id}</div>
-      <div>Title: {title}</div>
-      <div>check-in: {check_in}</div>
-      <div>accuracy: {accuracy}</div>
-      <div>communication: {communication}</div>
-      <div>cleanliness: {cleanliness}</div>
-      <div>location: {location}</div>
-      <div>value: {value}</div>
-      <div>hospitality: {hospitality}</div>
-      <div>stylish: {stylish}</div>
-      <div>sparkling clean: {sparkling_clean}</div>
-      <div>quick responses: {quick_responses}</div>
-      <div>amazing amenities: {amazing_amenities}</div>
+    <OuterContainer>
+      <InnerContainer>
+        <div>Check-in: {check_in}</div>
+        <div>Accuracy: {accuracy}</div>
+        <div>Communication: {communication}</div>
+        <div>Cleanliness: {cleanliness}</div>
+        <div>Location: {location}</div>
+        <div>Value: {value}</div>
+      </InnerContainer>
+      <MidLine />
+      <InnerContainer>
+        <div>Hospitality: {hospitality}</div>
+        <div>Stylish space: {stylish}</div>
+        <div>Sparkling clean: {sparkling_clean}</div>
+        <div>Quick responses: {quick_responses}</div>
+        <div>Amazing amenities: {amazing_amenities}</div>
+      </InnerContainer>
+    </OuterContainer>
   </WrappReviews>
 );
 
